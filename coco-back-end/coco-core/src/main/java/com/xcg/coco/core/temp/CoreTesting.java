eb8521da5b02943ef3367b089fb56273f04aec5e
ClementGib <c.gibertpro@gmail.com>
2021-11-22 00:03:19 +0100

    feature-30 : MEP utilitaires (interface & POJO) de base pour l'architecture et le developpment

--- coco-back-end/coco-core/src/main/java/com/xcg/coco/core/temp/CoreTesting.java
+++ coco-back-end/coco-core/src/main/java/com/xcg/coco/core/temp/CoreTesting.java
@@ -1,29 +1,56 @@
 package com.xcg.coco.core.temp;
 
+import java.util.Optional;
+
 import javax.annotation.PostConstruct;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.xcg.coco.core.category.CategoryDao;
+import com.xcg.coco.core.category.CategoryEntity;
+
+
+
 @Startup
 @Singleton
 public class CoreTesting {
+	
+	@Inject
+	CategoryDao categoryDAO;
+	
 	private static final Logger LOGGER = LoggerFactory.getLogger(CoreTesting.class);
 
 	@PostConstruct
 	public void init() {
-		LOGGER.info("INFO");
-		LOGGER.debug("DEBUG");
-		LOGGER.error("ERROR");
+		try {
+			CategoryEntity firstEntity = new CategoryEntity();
+			firstEntity.setName("Java");
+			firstEntity.setDescription("Category for java tutorials");
+			
+			categoryDAO.add(firstEntity);
+			LOGGER.info(firstEntity.getName() + " pesisted");
+			Optional<CategoryEntity> category = categoryDAO.findById("Java");
+			
+			if(category.isPresent()) {
+				LOGGER.info("found :" + category.get());
+				LOGGER.info("found :" + category.get().getDescription());
+			}
+			
+		} catch (Exception Exception) {
+			LOGGER.error("ERROR:", Exception);
+		}
+
 	}
 
 	@Schedule(hour = "*", minute = "*", second = "*/5", info = "Every 5 second timer")
 	public void tester() {
-		LOGGER.info("INFO");
-		LOGGER.debug("DEBUG");
-		LOGGER.error("ERROR");
+		
+		LOGGER.info("CoreTesting is running!");
+
 	}
 }
--- coco-back-end/coco-core/src/main/java/com/xcg/coco/core/tutorial/TutorialEntity.java
+++ coco-back-end/coco-core/src/main/java/com/xcg/coco/core/tutorial/TutorialEntity.java
@@ -1,5 +1,59 @@
 package com.xcg.coco.core.tutorial;
 
-public class TutorialEntity {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+//@Table(schema = "cocoapp", name = "tutorials", uniqueConstraints = { 
+//		@UniqueConstraint(columnNames = "tutorial_id"),
+//		@UniqueConstraint(columnNames = "category_name") })
+@Table(schema = "cocoapp", name = "tutorials", uniqueConstraints = @UniqueConstraint(columnNames = "tutorial_id")) 
+public class TutorialEntity implements Serializable {
+
+	/**
+	 * V0.0.1
+	 */
+	private static final long serialVersionUID = -5204051786157374314L;
+
+	@Id
+	@Column(name = "tutorial_id", nullable = false)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tutorials_tutorial_id_seq_gen")
+	@SequenceGenerator(name = "tutorials_tutorial_id_seq_gen", sequenceName = "tutorials_tutorial_id_seq", allocationSize = 1, initialValue = 1)
+	Integer id;
+
+	@Column(name = "title", nullable = false)
+	String title;
+
+//	@ManyToMany
+//	@JoinColumn(name = "category_name", nullable = false)
+//	Set<CategoryEntity> categories;
+
+	@Column(name = "author", nullable = false)
+	String author;
+
+	@Column(name = "description", nullable = false)
+	String description;
+
+	@Column(name = "image_name", nullable = false)
+	String imageName;
+
+	@Column(name = "like_count", nullable = false)
+	int likeCount;
+
+//	@OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+//	List<Post> posts;
 
 }
