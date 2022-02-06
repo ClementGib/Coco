8f5da6342afab776c4970e134ad72d95da2ccdb8
ClementGib <c.gibertpro@gmail.com>
2022-02-06 20:44:46 +0100

    Coco#57 : script to fix WSL and docker folder moved in coco-tools

---
+++ coco-back-end/coco-tools/script/wsl_online.ps1
@@ -0,0 +1,7 @@
+wsl --shutdown
+netsh winsock reset
+netsh int ip reset all
+netsh winhttp reset proxy
+ipconfig /flushdns
+netsh winsock reset
+shutdown /r
---
+++ coco-back-end/coco-tools/script/wsl_online.sh
@@ -0,0 +1,2 @@
+sudo ifconfig eth0 netmask 255.255.240.0
+sudo ip route add default via 172.17.0.1
