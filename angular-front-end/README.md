e336d7f5129aa98da346bcced06a0c323a554440
CodingClem <c.gibertpro@gmail.com>
2022-05-19 14:23:16 +0200

    feature#58: Mise en place page admin swaggerUI

--- angular-front-end/README.md
+++ angular-front-end/README.md
@@ -29,4 +29,5 @@ To get more help on the Angular CLI use `ng help` or go check out the [Angular C
 
 
 ## npm packages: 
+- swagger-ui
 - bootstrap-icons
--- angular-front-end/src/app/admin/admin.module.ts
+++ angular-front-end/src/app/admin/admin.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './components/admin.component';
 import { SharedModule } from '@shared/shared.module';
+import { SwaggerUiComponent } from './components/swagger-ui/swagger-ui.component';
 
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, SwaggerUiComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
